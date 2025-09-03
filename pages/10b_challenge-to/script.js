b92529b2fda79fb81dbc448aa8e0d49d41895d24
Sofia Matthaiaki <93294647+Sophie-Mat@users.noreply.github.com>
2025-09-03 14:25:55 +0300

    gsap.to() challenge completed

---
+++ pages/10b_challenge-to/script.js
@@ -0,0 +1,47 @@
+import gsap from 'gsap';
+
+const showToast1 = () => {
+    gsap.to('.toast.one', {
+        y: -120,
+        opacity: 1,
+        duration: 0.8,
+        ease: 'power4.out',
+        onComplete: () => {
+            gsap.to('.toast.one', {
+                delay: 1.5,
+                y: 0,
+                opacity: 0,
+                duration: 0.7,
+                ease: 'power2.in',
+                onComplete: () => {
+                    setTimeout(showToast2, 1000)
+                }
+
+            })
+        }
+    })
+}
+
+const showToast2 = () => {
+    gsap.to('.toast.two', {
+        y: -120,
+        opacity: 1,
+        duration: 0.8,
+        ease: 'power4.out',
+        onComplete: () => {
+            gsap.to('.toast.two', {
+                delay: 1.5,
+                y: 0,
+                opacity: 0,
+                duration: 0.7,
+                ease: 'power2.in',
+                onComplete: () => {
+                    setTimeout(showToast1, 1000)
+                }
+
+            })
+        }
+    })
+}
+
+showToast1();
